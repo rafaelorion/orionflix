10bf758db0e6e90f6beaa48cf419095ccfc00433
Rafael Orion <rafaelorion@gmail.com>
2020-08-17 00:39:41 -0300

    Aula 3 fim

--- src/components/pages/cadastro/categoria/index.js
+++ src/components/pages/cadastro/categoria/index.js
@@ -2,18 +2,31 @@ import React from 'react';
 import PageDefault from '../../../PageDefault';
 import { Link } from 'react-router-dom';
 
-function CadastroCategoria()
-{
-  return(
+function CadastroCategoria() {
+  return (
     <PageDefault>
       <h1>Cadastro de Categoria</h1>
 
-    <Link to="/">
-        Voltar para Home
-    </Link>
+      <form>
 
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
+
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+
+      <Link to="/">
+        Ir para home
+      </Link>
     </PageDefault>
-  ) 
+  )
 }
 
 export default CadastroCategoria;
