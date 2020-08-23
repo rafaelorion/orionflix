a5eb3b8c5c6fc1208faef1a679e76a208ed27c3b
Rafael Orion <rafaelorion@gmail.com>
2020-08-23 17:37:43 -0300

    Aula 5

--- src/pages/cadastro/video/index.js
+++ src/pages/cadastro/video/index.js
@@ -1,12 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PageDefault from '../../../components/PageDefault';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
+import useForm from '../../../hooks/useForm';
+import FormField from '../../../components/FormField';
+import Button from '.././../../components/Button';
+import videosRepository from '../../../repositories/videos';
+import categoriasRepository from '../../../repositories/categorias';
 
 function CadastroVideo()
 {
+  const history = useHistory();
+  const [categorias, setCategorias] = useState([]);
+  const sugestoesDeCategoria = categorias.map(({titulo}) => titulo );
+  const { handleChange, values } = useForm({ titulo: '', url: '', categoria: '',});
+
+  useEffect(() => {
+    categoriasRepository
+    .getAll()
+    .then((categorias) => {
+       setCategorias(categorias);
+    }); 
+  }, []);
+
   return(
     <PageDefault>
       <h1>Cadastro de Video</h1>
+
+      <form onSubmit={(event)=>{ 
+          event.preventDefault();
+
+          const categoriaEscolhida = categorias.find((categoria) => { return categoria.titulo === values.categoria; });
+
+          videosRepository.create({
+            titulo: values.titulo,
+            url: values.url,
+            categoriaId: categoriaEscolhida.id,
+          }).then(() => {
+            history.push('/');
+          });
+        }}>
+        
+          <FormField label="Titulo do Video" type="text" name="titulo" value={values.titulo} onChange={handleChange} />
+          <FormField label="Url do Video" type="text" name="url" value={values.url} onChange={handleChange} />
+          <FormField label="Categoria" type="text" name="categoria" value={values.categoria } suggestions={sugestoesDeCategoria} onChange={handleChange} />
+          
+          <Button type="submit">Cadastrar</Button>
+      </form>
 
     <Link to="/cadastro/categoria">
         Cadastrar Categoria
--- src/repositories/categorias.js
+++ src/repositories/categorias.js
@@ -2,6 +2,18 @@ import config from '../config';
 
 const URL_CATEGORIES = `${config.URL}/categorias`;
 
+function getAll() {
+    return fetch(`${URL_CATEGORIES}`)
+      .then(async (respostaDoServidor) => {
+        if (respostaDoServidor.ok) {
+          const resposta = await respostaDoServidor.json();
+          return resposta;
+        }
+  
+        throw new Error('Não foi possível pegar os dados :(');
+      });
+  }
+  
 function getAllWithVideos()
 {
     return fetch(`${URL_CATEGORIES}?_embed=videos`).then(async(response) => {
@@ -13,4 +25,4 @@ function getAllWithVideos()
     });   
 }
 
-export default {getAllWithVideos};
+export default { getAll, getAllWithVideos };
